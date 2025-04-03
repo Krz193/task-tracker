34079a84d588fc33866a101b6781b107e19ed4c5
nekoLin <krusiawanbudi1@gmail.com>
2025-04-03 14:22:48 +0800

    feat: update nav links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link>
+                        {{ __('') }}
+                    </x-nav-link>
                 </div>
             </div>
 
