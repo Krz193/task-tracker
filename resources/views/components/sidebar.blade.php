2cc8a04e695981fee73763e4bf669db7d5f4ac62
nekoLin <krusiawanbudi1@gmail.com>
2025-04-03 14:25:15 +0800

    feat: update dashboard layout

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,8 +1,29 @@
-<aside {{ $attributes->merge(['class' => 'default-class']) }}>
-    <div class="btn-cont flex">
-        <button 
-        class="bg-gray w-full text-left border-2 py-3 px-4 rounded-lg overflow-hidden">
-            Project scpssssssssssssssssssssssssssssssssssssssssssssssssssssss
+@props(['projects'])
+<aside {{ $attributes->merge(['class' => 'w-1/4']) }}>
+    <div class="container flex justify-between items-center mb-4">
+        <h1 class="capitalize mb-4 text-xl font-semibold">
+            {{ __('Projects List') }}
+        </h1>
+        <button
+            onclick="location.href='{{ route('project.store') }}'"
+            type="button"
+            class="capitalize text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+            + add new
         </button>
+    </div>
+    <div class="btn-cont">
+        @foreach ($projects as $project)
+        <button
+            onclick="location.href='/project/{{ $project->id }}'"
+            type="button"
+            class="w-full text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900
+                focus:ring-1 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-4 text-left
+                me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+
+            <div class="overflow-hidden text-ellipsis whitespace-nowrap">
+                {{ $project->project_name }}
+            </div>
+        </button>
+        @endforeach
     </div>
 </aside>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,19 +18,12 @@
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
             <!-- Page Content -->
-            <main class="container flex flex-row min-h-[200vh] w-100 relative">
-                <x-sidebar class="w-1/4 p-5 min-h-100 bg-white shadow static top-0 left-0"/>
-                <div class="flex-1 p-5 min-h-screen">
+            <main class="container flex flex-row w-full static h-screen">
+                <!-- Sidebar (selalu tampil di semua halaman) -->
+                <x-sidebar :projects="$projects" class="p-5 bg-white shadow sticky left-0 top-0" />
+
+                <div class="flex-1 p-5">
                     {{ $slot }}
                 </div>
             </main>
