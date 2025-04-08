9b10c509f4e747e13f913201f809ca10bf27a4c7
nekoLin <krusiawanbudi1@gmail.com>
2025-04-08 16:30:35 +0800

    feat: update routes

--- routes/web.php
+++ routes/web.php
@@ -3,30 +3,32 @@
 use App\Http\Middleware\ProjectsList;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\DashboardController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/dashboard', function () {
     return redirect('/');
 });
 
 Route::get(
-    '/', [ProjectController::class, 'index']
+    '/', [DashboardController::class, 'index']
 )->middleware(['auth', 'verified', ProjectsList::class])->name('dashboard');
 
 Route::middleware(['auth', 'verified', ProjectsList::class])->group(function() {
-    Route::get('/project/{id}', [ProjectController::class, 'find'])->name('project.index');
-
-    Route::get('/project', function() { return view('form_project'); })->name('project.store');
-    Route::post('/project', [ProjectController::class, 'store'])->name('project.store');
-
-    Route::get('/project/{project}/edit', [ProjectController::class, 'edit'])->name('project.edit');
-    Route::put('/project/{project}', [ProjectController::class, 'update'])->name('project.update');
-
-    Route::delete('/project/{project}', [ProjectController::class, 'destroy'])->name('project.destroy');
+    Route::prefix('project')->middleware(['auth'])->group(function() {
+        Route::get('/{id}', [ProjectController::class, 'find'])->name('project.index');
+        Route::get('/', function() { return view('form_project'); })->name('project.store');
+        Route::post('/', [ProjectController::class, 'store'])->name('project.store');
+        Route::get('/{project}/edit', [ProjectController::class, 'edit'])->name('project.edit');
+        Route::put('/{project}', [ProjectController::class, 'update'])->name('project.update');
+        Route::delete('/{project}', [ProjectController::class, 'destroy'])->name('project.destroy');
+    });
 
     Route::prefix('tasks')->middleware(['auth'])->group(function() {
-        Route::get('/tasks/create/{project}', [TaskController::class, 'create'])->name('tasks.create');
+        Route::get('/create/{project}', [TaskController::class, 'create'])->name('tasks.create');
         Route::post('/store/{project}', [TaskController::class, 'store'])->name('tasks.store');
         Route::get('/edit/{task}', [TaskController::class, 'edit'])->name('tasks.edit');
         Route::put('/update/{task}', [TaskController::class, 'update'])->name('tasks.update');
@@ -34,6 +36,8 @@ Route::middleware(['auth', 'verified', ProjectsList::class])->group(function() {
         Route::put('/update-status/{task}', [TaskController::class, 'updateStatus'])->name('tasks.updateStatus');
     });
 });
+
+Route::resource('users', UserController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
