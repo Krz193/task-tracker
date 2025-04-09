3a0db233bd5dfa42f8f156cd5823c86255be34c5
nekoLin <krusiawanbudi1@gmail.com>
2025-04-09 15:30:55 +0800

    feat: finishing user dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,26 +7,50 @@ use App\Enums\UserRole;
 use App\Models\Project;
 use App\Models\Task;
 use App\Models\User;
+use App\Services\QuoteService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        // cek apakah yang login adalah admin
         $user = Auth::user();
         $isAdmin = $user->role === UserRole::ADMIN;
 
-        // dd(Task::whereHas('project', fn($q) => 
-        //     $q->where('user_id', $user->id)
-        // )->toSql());
+        // ambil total task dari semua project user yang login
+        $totalTask = Task::whereHas('project', fn($q) => 
+        $q->where('user_id', '=', Auth::id()))
+        ->count();
 
+        // ambil total task yang berstatus 'done' dari semua project
+        $totalDoneTasks = Task::whereHas('project', fn($q) => 
+        $q->where('user_id', '=', Auth::id())
+        ->where('status', '=', TaskStatus::DONE))
+        ->count();
 
+        // hitung persentase task yang dikerjakan user yang login
+        $tasksPercentage = ($totalTask !== 0) 
+        ? round(($totalDoneTasks/$totalTask) * 100, 2)
+        : 0;
+
+        // ambil quote sesuai persentase
+        $quote = QuoteService::getQuote($tasksPercentage);
+
+        // deklarasi array kosong untuk store data
         $array = [];
 
+        // cek kondisi role user yang login
         if($isAdmin) {
+
+            /**
+             * jika admin isi dengan data yang diperlukan di 
+             * dashboard admin
+            */
             $array = [
                 'isAdmin' => true,
                 'totalUsers' => User::count(),
@@ -47,16 +71,37 @@ class DashboardController extends Controller
                 ->first(),
             ];
         } else {
+
+            /**
+             * jika bukan admin isi dengan data yang diperlukan di 
+             * dashboard user
+            */
             $array = [
                 'isAdmin' => false,
-                'totalProjects' => Project::where('user_id', $user->id)->count(),
-                'totalTasks' => Task::with('project')
-                ->whereHas('project', function ($q)use ($user) { 
-                    $q->where('user_id', $user->id);
-                })->count(),
+                'totalProjects' => Project::where('user_id', '=', Auth::id())
+                ->count(),
+                'topProject' => Project::with(['tasks' => function($q) {
+                    $q->where('status', 'done');
+                }])
+                ->withCount(['tasks as tasks_done' => function($q) {
+                    $q->where('status', 'done');
+                }])
+                ->where('user_id', '=', Auth::id())
+                ->orderByDesc('tasks_done')
+                ->first(),
+                'totalTasks' => $totalTask,
+                'totalDoneTasks' => $totalDoneTasks,
+                'tasksPercentage' => $tasksPercentage,
+                'quote' => $quote,
             ];
         }
 
+        // dd($array);
+
+        /**
+         * jadikan json object semua nested array,
+         * lalu kembalikan ke array semua json object sampai nested json nya
+         */
         $data = json_decode(json_encode($array));
 
         return view('dashboard', compact('data'));
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    @admin
     {{-- dashboard admin --}}
     <div class="flex mb-5 gap-4 flex-wrap">
         <div class="w-[calc((1/3*100%)-1rem)] p-6 flex flex-col h-[25dvh] bg-white border border-gray-200 rounded-lg shadow-sm">
@@ -46,5 +47,66 @@
         </div>
     </div>
 
+    @else
+    {{-- dashboard user --}}
+    <blockquote class="text-3xl capitalize italic font-bold text-gray-900 mb-2">
+        <p>
+            {{ $data->quote }}.
+        </p>
+    </blockquote>
+    <h3 class="mb-5 text-xl">
+        {{ __(ucfirst(strtolower("you have done $data->tasksPercentage% of your tasks"))) }},
+        <span class="italic">
+            @php
+                $quote = ($data->tasksPercentage>75) 
+                ? 'congrats' 
+                : (
+                        ($data->tasksPercentage>50) 
+                        ? 'keep it up' 
+                        : 'break a leg'
+                    )
+            @endphp
+            {{ ucfirst(strtolower($quote)) }}!
+        </span>
+    </h3>
+    <div class="flex mb-5 gap-4 flex-wrap">
+        <div class="w-[calc((1/3*100%)-1rem)] p-6 flex flex-col h-[25dvh] bg-white border border-gray-200 rounded-lg shadow-sm">
+            <h1 class="text-2xl flex-1 font-bold tracking-tight text-gray-900 capitalize">
+                total projects
+            </h1>
+            <p class="font-normal text-gray-700 text-end">
+                {{ $data->totalProjects }}
+            </p>
+        </div>
+        <div class="w-[calc((1/3*100%)-1rem)] p-6 flex flex-col h-[25dvh] bg-white border border-gray-200 rounded-lg shadow-sm">
+            <h1 class="text-2xl flex-1 font-bold tracking-tight text-gray-900 capitalize">
+                total tasks
+            </h1>
+            <p class="font-normal text-gray-700 text-end">
+                {{ $data->totalTasks }}
+            </p>
+        </div>
+        <div class="w-[calc((1/3*100%)-1rem)] p-6 flex flex-col h-[25dvh] bg-white border border-gray-200 rounded-lg shadow-sm">
+            <h1 class="text-2xl flex-1 font-bold tracking-tight text-gray-900 capitalize">
+                total done tasks
+            </h1>
+            <p class="font-normal text-gray-700 text-end">
+                {{ $data->totalDoneTasks }}
+            </p>
+        </div>
+        <div class="w-[calc((1*100%)-1rem)] p-6 flex flex-col h-[25dvh] bg-white border border-gray-200 rounded-lg shadow-sm">
+            <h1 class="text-2xl flex-1 font-bold tracking-tight text-gray-900 capitalize">
+                Top project
+            </h1>
+            <p class="font-normal text-gray-700 text-end">
+                {{ $data->topProject->project_name }} 
+                <span>
+                    ({{ $data->topProject->tasks_done }}
+                    task{{ ($data->topProject->tasks_done>1 ? 's' : '') }} done)
+                </span>
+            </p>
+        </div>
+    </div>
+    @endadmin
 
 </x-app-layout>
