c904bbb363f13bae2f2206a0f36b273ca9138d1c
nekoLin <krusiawanbudi1@gmail.com>
2025-04-11 17:52:13 +0800

    feat: add edit button to task

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,6 +1,5 @@
 <x-app-layout>
-    {{-- {{ dd($project) }}
-    --}}
+    {{-- {{ dd(route('logout')) }} --}}
 
     <div class="container flex flex-col w-full">
         <div class="container flex flex-row justify-between items-center">
@@ -79,6 +78,8 @@
                 </form>
             </div>
         </div>
+
+        {{-- tasks list --}}
         <div class="container flex mb-4 mt-2">
             <!-- Cek apakah proyek memiliki gambar -->
             @if ($project->image)
@@ -135,7 +136,28 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="action self-end">
+                        <div class="action flex gap-2 self-end">
+                            <button
+                                onclick="location.href='{{ route('tasks.edit', $task) }}'"
+                                type="button"
+                                class="capitalize border border-yellow-600 flex items-center text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm py-2 px-4 text-center dark:focus:ring-yellow-900">
+                                {{-- svg icon edit --}}
+                                <svg
+                                    class="w-6 h-6"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24">
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z"
+                                        clip-rule="evenodd"/>
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z"
+                                        clip-rule="evenodd"/>
+                                </svg>
+                            </button>
                             <form
                                 action="{{ route('tasks.destroy', $task) }}"
                                 method="POST"
@@ -143,7 +165,7 @@
                                 @csrf @method('DELETE')
                                 <button
                                     type="submit"
-                                    class="capitalize border border-red-900 flex items-center text-white bg-red-700 hover:bg-red-800 rounded-lg text-center py-2 px-4   ">
+                                    class="capitalize border border-red-900 flex items-center text-white bg-red-700 hover:bg-red-800 rounded-lg text-center py-2 px-4">
                                     {{-- svg icon trashbin --}}
                                     <svg
                                         class="w-6 h-6"
