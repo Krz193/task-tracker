75c1d827e611d8cd1382ec9d8af35ec97f28f94a
nekoLin <krusiawanbudi1@gmail.com>
2025-04-11 11:31:05 +0800

    feat: register discord service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use App\Enums\UserRole;
 use App\Models\Project;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,15 +26,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // URL::forceHttps();
+        // URL::forceScheme('https');
+
         View::composer('layouts.app', function ($view) {
             $user = Auth::user();
             $projects = $user ? Project::where('user_id', $user->id)->get() : collect();
             $view->with('projects', $projects);
         });
-
         
         Blade::if('admin', function () {
             return Auth::check() && Auth::user()->role === UserRole::ADMIN;
+        });
+
+        Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
+            $event->extendSocialite('discord', \SocialiteProviders\Discord\Provider::class);
         });
     }
 }
